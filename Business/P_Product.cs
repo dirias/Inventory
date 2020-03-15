fd09cac5d131fec9ff0b2aae56435a899cd39a4e
Didier <didier.i.m.r@hotmail.com>
2020-03-14 21:59:43 -0600

    BusinessLayer done

--- Business/P_Product.cs
+++ Business/P_Product.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using DataAccess;
+using Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business
 {
     public class P_Product
     {
+        public List<ProductEntity> ProductList()
+        {
+            using (var db = new InventaryContext())
+            {
+                return db.Products.ToList();
+            }
+        }
+        public void CreateProduct(ProductEntity oProduct)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Products.Add(oProduct);
+                db.SaveChanges();
+            }
+        }
+        public void UpdateProduct(ProductEntity oProduct)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Products.Update(oProduct);
+                db.SaveChanges();
+            }
+        }
     }
 }
